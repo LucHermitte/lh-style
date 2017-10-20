ac384da727617a24d3ae64a94e9a14b110a62174
Luc Hermitte <luc.hermitte@gmail.com>
2017-10-20 17:34:37 +0000

    DOC: remove invalid module

--- doc/conf.py
+++ doc/conf.py
@@ -32,8 +32,7 @@
 extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
-    'm2r'
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
